9ddd66ff4791a1cad5ba89642734338a47d9af61
mjwilson-bjss <140594334+mjwilson-bjss@users.noreply.github.com>
2024-01-03 13:43:43 +0000

    Print out hostname

--- src/main/java/com/bjss/mjwilson/gha/Gha.java
+++ src/main/java/com/bjss/mjwilson/gha/Gha.java
@@ -1,12 +1,17 @@
 package com.bjss.mjwilson.gha;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Gha {
 
     public int seven() {
         return 7;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         System.out.println("hello world");
+        System.out.println(InetAddress.getLocalHost().getHostName());
     }
+
 }
