61bd17ab462c77d9350d7d25a6ba6e0cd5de0f15
mjwilson-bjss <140594334+mjwilson-bjss@users.noreply.github.com>
2024-01-03 10:56:06 +0000

    Add method for testing

--- src/main/java/com/bjss/mjwilson/gha/Gha.java
+++ src/main/java/com/bjss/mjwilson/gha/Gha.java
@@ -2,6 +2,10 @@ package com.bjss.mjwilson.gha;
 
 public class Gha {
 
+    public int seven() {
+        return 7;
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
