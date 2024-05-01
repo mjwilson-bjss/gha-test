0ce7e70893905128e8d8c3c4c21cf462ee9f482a
Matthew Wilson <matthew.wilson@bjss.com>
2024-05-01 09:29:30 +0100

    CodeQL experiments

--- src/main/java/com/bjss/mjwilson/springboot/controllers/HelloWorldController.java
+++ src/main/java/com/bjss/mjwilson/springboot/controllers/HelloWorldController.java
@@ -1,14 +1,15 @@
 package com.bjss.mjwilson.springboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String index() {
-        return "Spring Boot hello world controller";
+    public String index(@RequestParam final String message) {
+        return "Spring Boot hello world controller " + message;
     }
 
 }
